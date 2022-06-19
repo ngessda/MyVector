3f5fd83928168d2d1f769eded2f4d323c6df4c51
Igor Kind <86460417+ngessda@users.noreply.github.com>
2022-06-19 17:24:34 +0900

    none

--- MyVector/MyVectory.cs
+++ MyVector/MyVectory.cs
@@ -8,51 +8,155 @@ namespace MyVector
 {
     public class MyVectory
     {
-        public double a;
-        public double b;
-        public double lengthVector;
-
-        public MyVectory(double a, double b)
+        public double[] a;
+        private int length;
+        public int lengthVector
         {
-            this.a = a;
-            this.b = b;
-            lengthVector = LengthVectory();
+            get { return length; }
         }
 
-        private double LengthVectory()
+        public MyVectory()
         {
-            double lengthVectory = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
-            return lengthVectory;
+            a = new double[0];
+        }
+
+        public MyVectory(int length)
+        {
+            this.length = length;
+            CreateRandom(length);
+        }
+        public MyVectory(double[] z)
+        {
+            length = z.Length;
+            a = z;
+        }
+        public MyVectory this [int index]
+        {
+            get
+            {
+                return this[index];
+            }
+        }
+
+        private double[] CreateRandom(int length)
+        {
+            Random random = new Random();
+            a = new double[length];
+            for (int i = 0; i < length; i++)
+            {
+                a[i] = random.NextDouble();
+            }
+            return a;
+        }
+
+        public static int[] Arrange(int n)
+        {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = i;
+            }
+            return a;
+        }
+        public static int[] Zeros(int n)
+        {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = 0;
+            }
+            return a;
+        }
+        public static int[] Ones(int n)
+        {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = 1;
+            }
+            return a;
+        }
+        public double Norm()
+        {
+            double res = 0;
+            for (int i = 0; i < length; i++)
+            {
+                res += Math.Pow(a[i], 2);
+            }
+            return Math.Sqrt(res);
         }
 
         public static MyVectory operator +(MyVectory a, MyVectory b)
         {
-            MyVectory vs = new MyVectory(a.a + b.a, a.b + b.b);
+            MyVectory vs = a;
+            for (int i = 0; i < a.length; i++)
+            {
+                vs.a[i] += b.a[i];
+            }
             return vs;
         }
         
         public static MyVectory operator -(MyVectory a, MyVectory b)
         {
-            MyVectory vs = new MyVectory(a.a - b.a, a.b - b.b);
+            MyVectory vs = a;
+            for (int i = 0; i < a.length; i++)
+            {
+                vs.a[i] -= b.a[i];
+            }
             return vs;
         }
 
         public static MyVectory operator *(MyVectory a, double b)
         {
-            MyVectory vs = new MyVectory(a.a * b, a.b * b);
+            MyVectory vs = a;
+            for (int i = 0; i < a.length; i++)
+            {
+                vs.a[i] *= b;
+            }
             return vs;
         }
 
         public static MyVectory operator *(double b, MyVectory a)
         {
-            MyVectory vs = new MyVectory(a.a * b, a.b * b);
+            MyVectory vs = a;
+            for (int i = 0; i < a.length; i++)
+            {
+                vs.a[i] *= b;
+            }
             return vs;
         }
-
-        public double ScaleMultiplyVectory(MyVectory a, MyVectory b)
+        public double ScalarTimes(MyVectory b)
         {
-            double vs = a.a * b.a + a.b * b.b;
-            return vs;
+            if(a.Length != b.length)
+            {
+                throw new ArgumentException("Ты совсем даун?");
+            }
+            else
+            {
+                double result = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    result += a[i] * b.a[i];
+                }
+                return result;
+            }
+        }
+
+        public static double ScalarTimes(MyVectory a, MyVectory b)
+        {
+            if(a.length != b.length)
+            {
+                throw new ArgumentException("Ты совсем даун?");
+            }
+            else
+            {
+                double result = 0;
+                for (int i = 0; i < a.length; i++)
+                {
+                    result += a.a[i] * b.a[i];
+                }
+                return result;
+            }
         }
     }
 }
